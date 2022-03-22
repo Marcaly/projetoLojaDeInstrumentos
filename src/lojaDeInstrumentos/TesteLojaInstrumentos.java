74cd95d0d12d011e50e5526e96880ed796f402bb
Marcaly <byelmarcal@hotmail.com>
2022-03-22 20:42:32 -0300

    outout

---
+++ src/lojaDeInstrumentos/TesteLojaInstrumentos.java
@@ -0,0 +1,11 @@
+package lojaDeInstrumentos;
+
+public class TesteLojaInstrumentos {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo à maior loja de instrumentos de Brasilia! ");
+
+
+
+
+    }
+}
